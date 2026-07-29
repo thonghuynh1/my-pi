03bc7f7babe878c39e381ea7b5943581233d098b
Thong Huynh Minh <thong.huynh.minh@aspia.se>
2026-07-29 15:37:16 +0200

    no need this one

--- extensions/aiknow/index.ts
+++
@@ -1,4 +0,0 @@
-// my-pi entrypoint for the official aiKnow adapter.
-// Keep the adapter implementation with aiKnow and load it through this package.
-export { piExtension } from "../../../aiKnow/integrations/pi/aiknow/index.ts";
-export { default } from "../../../aiKnow/integrations/pi/aiknow/index.ts";

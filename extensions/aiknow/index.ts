b5861782c346127900c076d0fc83868e8d8944c8
Thong Huynh Minh <thong.huynh.minh@aspia.se>
2026-07-30 06:08:52 +0200

    add pi

---
+++ extensions/aiknow/index.ts
@@ -0,0 +1,18 @@
+import { existsSync } from "fs";
+
+const DEFAULT_AIKNOW_PATH =
+  "C:/Hackathon/aiKnow/aiKnow/integrations/pi/aiknow/index.ts";
+
+const AIKNOW_PATH = process.env.AIKNOW_PATH ?? DEFAULT_AIKNOW_PATH;
+
+export const piExtension = { id: "aiknow" };
+
+export default async function (pi: unknown) {
+  if (!existsSync(AIKNOW_PATH)) {
+    return;
+  }
+  const mod = await import(AIKNOW_PATH);
+  if (typeof mod.default === "function") {
+    await mod.default(pi);
+  }
+}
